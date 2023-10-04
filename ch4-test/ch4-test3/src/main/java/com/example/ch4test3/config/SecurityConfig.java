7ad66359439ef0ca3ed1bbeba57301b14525471e
lsy3709 <lsy3709@naver.com>
2023-10-04 09:50:32 +0900

    231004 1교시 저장

--- ch4-test/ch4-test3/src/main/java/com/example/ch4test3/config/SecurityConfig.java
+++ ch4-test/ch4-test3/src/main/java/com/example/ch4test3/config/SecurityConfig.java
@@ -23,20 +23,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // 기본 인증 방식, 로그인 폼 방식을 이용. id/pw 이용하겠다.
+        // Bear , 토큰 방식, 예) jwt 많이 사용함. 서버에 부담 감소위해서.
+        // 기본적으로, 로그인 기능과, 로그아웃 기능을 가지고 있음.
+        // 만약, 개발자가 다른 로그인 폼을 이용한다면, 아래 설정 처럼 변경가능.
+        //
         http.formLogin()
+                // 기본적인 로그인 폼 양식을 따로 지정한 경우.
                 .loginPage("/members/login")
+                // 로그인 성공시 이동할 페이지.
                 .defaultSuccessUrl("/")
+                // 인증의 절차를, 멤버의 이메일을 기준으로 설정.
                 .usernameParameter("email")
+                // 인증 실패시 이동할 페이지.
                 .failureUrl("/members/login/error")
                 .and()
                 .logout()
+                // 로그 아웃시 이용이 될 페이지 설정.
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                // 로그 아웃 성고시 이동할 페이지
                 .logoutSuccessUrl("/")
         ;
 
         http.authorizeRequests()
+                // 인증 및 인가 상관없이 누구든지 접근이 가능한 페이지 설정.
                 .mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                // 인가가 관리자만 접근이 가능한 페이지 설정.
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
+                // 그 외 모든 페이지는 인증이 되어야만 접근이 가능함.
                 .anyRequest().authenticated()
         ;
 
