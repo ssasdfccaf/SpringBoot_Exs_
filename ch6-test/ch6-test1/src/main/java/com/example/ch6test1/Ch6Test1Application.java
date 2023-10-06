abc349873a5aa783097ff93dd264dc733cb4c997
lsy3709 <lsy3709@naver.com>
2023-10-06 14:59:10 +0900

    231006 5교시 저장3

---
+++ ch6-test/ch6-test1/src/main/java/com/example/ch6test1/Ch6Test1Application.java
@@ -0,0 +1,13 @@
+package com.example.ch6test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch6Test1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch6Test1Application.class, args);
+    }
+
+}
